155cea3fbd159923df0b2938b28131c7573d8a19
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2022-05-08 19:56:43 +0200

    Complete mail notification support.

--- src/main/java/fr/xanode/horn/providers/communication/CommunicationProvider.java
+++ src/main/java/fr/xanode/horn/providers/communication/CommunicationProvider.java
@@ -2,12 +2,87 @@ package fr.xanode.horn.providers.communication;
 
 import fr.xanode.horn.notification.Notification;
 import fr.xanode.horn.notification.NotificationState;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.PriorityBlockingQueue;
 
-public interface CommunicationProvider {
-    void sendNotification(Notification notification);
-    Notification getLastNotification();
-    List<Notification> getLastNotifications(int numbers);
-    NotificationState getNotificationState(Notification notification);
+public class CommunicationProvider {
+
+    private static Logger logger = LogManager.getLogger(CommunicationProvider.class);
+
+    private final Properties properties;
+
+    private PriorityBlockingQueue<Notification> notificationQueue;
+
+    public CommunicationProvider(Properties properties) {
+        this.properties = properties;
+        this.notificationQueue = new PriorityBlockingQueue<>();
+    }
+
+    public void loadNotification(Notification notification) {
+        notification.setNotificationSate(NotificationState.PENDING);
+        this.notificationQueue.add(notification);
+        logger.trace("Notification " + notification.hashCode() + " loaded.");
+    }
+
+    public void removeNotification(Notification notification) {
+        if (notification.getNotificationState() != NotificationState.SENT) {
+            logger.warn("Remove unsent notification " + notification.hashCode() + " !");
+        } else {
+            logger.info("Remove notification " + notification.hashCode() + ".");
+        }
+        this.notificationQueue.remove(notification);
+    }
+
+    public void sendNotification(Notification notification) {
+        logger.info("Send notification : " + notification.hashCode());
+        if (!this.notificationQueue.contains(notification)) {
+            logger.error("Notification " + notification.hashCode() + " not loaded or already sent!");
+            throw new IllegalArgumentException("Notification not loaded or already sent!");
+        }
+        if (notification.getNotificationState() == NotificationState.SENDING || notification.getNotificationState() == NotificationState.SENT) {
+            logger.error("Notification " + notification.hashCode() + " already sent!");
+            throw new IllegalArgumentException("Notification already sent!");
+        }
+        // Logic implemented by inheritance
+    }
+
+    public Notification getHeadNotification() {
+        return notificationQueue.peek();
+    }
+
+    public List<Notification> getLastNotifications(int numbers) {
+        List<Notification> lastNotifications = new ArrayList<>();
+        Iterator<Notification> notificationQueueIterator = this.notificationQueue.iterator();
+        int i = 0;
+        while (notificationQueueIterator.hasNext() && i < numbers) {
+            lastNotifications.add(notificationQueueIterator.next());
+        }
+        return lastNotifications;
+    }
+
+    public void flushNotificationQueue() {
+        logger.info("Flush notification queue requested.");
+        while (this.notificationQueue.peek() != null) {
+            this.sendHeadNotification();
+        }
+    }
+
+    public void sendHeadNotification() {
+        logger.info("Send first pending notification.");
+        if (this.notificationQueue.peek() == null) {
+            logger.error("Trying to send first pending notification but the queue is empty!");
+            throw new IllegalArgumentException("Empty queue!");
+        }
+        this.sendNotification(this.notificationQueue.peek());
+    }
+
+    public Properties getProperties() {
+        return properties;
+    }
 }
---
+++ src/main/java/fr/xanode/horn/providers/communication/mail/MailCommunicationProvider.java
@@ -0,0 +1,57 @@
+package fr.xanode.horn.providers.communication.mail;
+
+import fr.xanode.horn.notification.Notification;
+import fr.xanode.horn.notification.NotificationState;
+import fr.xanode.horn.providers.communication.CommunicationProvider;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class MailCommunicationProvider extends CommunicationProvider {
+
+    private static Logger logger = LogManager.getLogger(MailCommunicationProvider.class);
+
+    MailCommunicationProvider(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public void sendNotification(Notification notification) {
+        super.sendNotification(notification);
+        // Setting notification state to sending
+        notification.setNotificationSate(NotificationState.SENDING);
+        // Initiate connection
+        String username = this.getProperties().getProperty("username");
+        String password = this.getProperties().getProperty("password");
+        Session session = Session.getInstance(this.getProperties(), new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("horn@xanode.fr"));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(this.getProperties().getProperty("recipient")));
+            message.setSubject(notification.getSubject());
+
+            MimeBodyPart mimeBodyPart = new MimeBodyPart();
+            mimeBodyPart.setContent(notification.toPlainText(), "text/html; charset=utf-8");
+
+            Transport.send(message);
+        } catch (MessagingException e) {
+            notification.setNotificationSate(NotificationState.FAILED);
+            logger.error("Failed to send notification " + notification.hashCode() + ".");
+        }
+        // Set notification state to sent and remove it from notification queue
+        notification.setNotificationSate(NotificationState.SENT);
+        this.removeNotification(notification);
+    }
+
+}
